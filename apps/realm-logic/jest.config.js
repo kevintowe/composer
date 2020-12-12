4e81eb2f5678a05ea57867cb7c02fea62dc14de3
kevin <kevintowe27@gmail.com>
2020-12-12 16:51:35 -0600

    generated microservices

---
+++ apps/realm-logic/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'realm-logic',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/realm-logic',
+};
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/oauth',
+    '<rootDir>/apps/api-client',
+    '<rootDir>/apps/realm-logic',
+    '<rootDir>/apps/token',
+    '<rootDir>/apps/webhook',
+    '<rootDir>/apps/default',
+  ],
+};
