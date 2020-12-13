eac6babb392594bb3f9376c586fc31fa37d609bf
kevin <kevintowe27@gmail.com>
2020-12-13 09:30:42 -0600

    developed `dev-proxy`, added dev-dependency

--- apps/dev-proxy/src/main.ts
+++ apps/dev-proxy/src/main.ts
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+import * as express from 'express';
+import * as httpProxy from 'express-http-proxy';
+const app = express();
+const host = `http://localhost`;
+
+const oauthServiceProxy = httpProxy(`${host}:3010`);
+const webhookServiceProxy = httpProxy(`${host}:3040`);
+
+// Authentication
+app.use((req, res, next) => {
+  // TODO: authentication logic
+  next();
+});
+
+// Proxy request
+app.get('/oauth', (req, res, next) => {
+  oauthServiceProxy(req, res, next);
+});
+app.post('/webhook', (req, res, next) => {
+  webhookServiceProxy(req, res, next);
+});
+
+// Determine port and listen for request
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Dev Proxy listening at ${host}:${port}`);
+});
