71fb0af4870bc88d95a2ac4aa9c738a82480e3be
kevin <kevintowe27@gmail.com>
2020-12-13 09:58:11 -0600

    oauth main.ts

--- apps/oauth/src/main.ts
+++ apps/oauth/src/main.ts
@@ -3,18 +3,23 @@
  * This is only a minimal backend to get started.
  */
 
+// THIS LINE MUST RUN FIRST!!!
+if (process.env.NODE_ENV !== 'production')
+  require('dotenv').config({ path: 'apps/oauth/src/.env' });
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+import { OAuthModule } from './app/oauth.module';
+
+console.log(`Testing value is: ${process.env.TEST}`);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create(OAuthModule);
+
   const port = process.env.PORT || 3010;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/');
   });
 }
 
